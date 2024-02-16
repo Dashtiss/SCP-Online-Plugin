ac6943a6229eff71c5241aec3d7ea4ab3b1b8eef
Dashtiss <brandontisserand@outlook.com>
2024-02-15 21:35:46 -0600

    Add project files.

---
+++ SCP-Online-Plugin/Config.cs
@@ -0,0 +1,22 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_Online_Plugin
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+
+        [Description("ServerID that will be sent to the database, don't change this cause it could break things")]
+        public int ServerID { get; set; }
+
+
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ SCP-Online-Plugin/SCP-Online.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace SCP_Online_Plugin
+{
+    public class SCP_Online : Plugin<Config>
+    {
+        public override string Author => "Dashtiss";
+        public override string Name => "Scp: Online";
+        public override Version Version => new Version(0, 0, 1);
+
+
+    }
+}
